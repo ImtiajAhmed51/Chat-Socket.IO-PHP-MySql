a5315a7c6f9846b786695d28291776270793ff49
Imtiaj Ahmed Anik <imtiajahmedanik@gmail.com>
2024-04-21 22:41:21 +0600

    error fixed

--- app/src/main/java/com/example/chat/ui/fragment/home/ChatUserFragment.java
+++ app/src/main/java/com/example/chat/ui/fragment/home/ChatUserFragment.java
@@ -1,21 +1,25 @@
 package com.example.chat.ui.fragment.home;
-
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.chat.R;
+import com.example.chat.databinding.FragmentChatUserBinding;
 
 public class ChatUserFragment extends Fragment {
+    private FragmentChatUserBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_user, container, false);
+        binding = FragmentChatUserBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
